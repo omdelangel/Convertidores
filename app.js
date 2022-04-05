06a204402e1dfd4f2056c26539f6dab4f65c8ce5
Oscar Moreno del Angel <omdelangel@gmail.com>
2022-04-04 20:10:54 -0500

    Implementa Notificaciones bajo Gestor de Mensajes

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ var mailOptions = {
     ]
 
 };
-
+/*
 // Proceso de envio de recibos
 cron.schedule('20 5 23 * * *',() => {
     console.log(new Date().toString() + " Corre proceso Obtener información para emision de recibos - email");
@@ -169,9 +169,103 @@ cron.schedule('20 5 23 * * *',() => {
 
 
 });
+*/
+
+// -----------------------------------------------------------------
+// Proceso de envio de recibos
+// -----------------------------------------------------------------
+cron.schedule('20 5 23 * * *',() => {
+    console.log(new Date().toString() + " Corre proceso Obtener información para emision de recibos - email");
+    //console.log(`${BASE.URL}`);
+    //let dia = dateFmt(new Date(), "yyyy-mm-dd");
+    //let dia = "2021-10-15";
+    //console.log(dia);
+
+    obtenInformacionMensajes(connection, {tipoNotifica: "5"} ,result => {
+        //obtenDatosRecibos(connection, {fecha: dia} ,result => {
+        console.log("dentro de Notificaciones " + __dirname);
+        const logName = "RecibosMail_" + new Date().toString().split(" ").join("_")+".json";
+        console.log(result);
+        var DatosRecibos = JSON.stringify(result);    
+        let jsonParsedArray = JSON.parse(DatosRecibos);
+        
+        for (let index = 0; index < jsonParsedArray[0].length; index++) {
+            const element = jsonParsedArray[0][index];
+            
+            mailOptions.to = element.Detalle.email;  
+            mailOptions.subject = "Recibo de consumo " + element.Detalle.NumTicket;
+            mailOptions.context = element.Detalle;
+            mailOptions.template = 'recibo'
+            console.log(mailOptions);
+            console.log("--------------------------------------");
+            transporter.sendMail(mailOptions, (err, info) => {
+                if (err) {
+                    console.log('Correo inválido' + err.message);
+                } else {
+                    console.log("Correo enviado");
+                    
+                    marcaMensajeEnviado(connection, {IdMensaje: element.IdMensajeria } ,result => {
+                        console.log("marcando notificación como enviado");
+                        console.log(JSON.stringify(result));
+                    
+                    });
+                }
+            });
+        }
+    });
+
+
+});
+
+// -----------------------------------------------------------------
+// Proceso de envio de comprobante de pago (no consumo)
+// -----------------------------------------------------------------
+cron.schedule('20 5 23 * * *',() => {
+    console.log(new Date().toString() + " Corre proceso Obtener información para emision de recibos - email");
+    //console.log(`${BASE.URL}`);
+    //let dia = dateFmt(new Date(), "yyyy-mm-dd");
+    //let dia = "2021-10-15";
+    //console.log(dia);
+
+    obtenInformacionMensajes(connection, {tipoNotifica: "8"} ,result => {
+        //obtenDatosRecibos(connection, {fecha: dia} ,result => {
+        console.log("dentro de Recibo de Pago Pendiente " + __dirname);
+        const logName = "ReciboPagoMail_" + new Date().toString().split(" ").join("_")+".json";
+        console.log(result);
+        var DatosComprobante = JSON.stringify(result);    
+        let jsonParsedArray = JSON.parse(DatosComprobante);
+        
+        for (let index = 0; index < jsonParsedArray[0].length; index++) {
+            const element = jsonParsedArray[0][index];
+            
+            mailOptions.to = element.Detalle.email;  
+            mailOptions.subject = "Recibo de Pago (No Condumo) " + element.Detalle.NumTicket;
+            mailOptions.context = element.Detalle;
+            mailOptions.template = 'reciboPagoPendiente'
+            console.log(mailOptions);
+            console.log("--------------------------------------");
+            transporter.sendMail(mailOptions, (err, info) => {
+                if (err) {
+                    console.log('Correo inválido' + err.message);
+                } else {
+                    console.log("Correo enviado");
+                    
+                    marcaMensajeEnviado(connection, {IdMensaje: element.IdMensajeria } ,result => {
+                        console.log("marcando notificación como enviado");
+                        console.log(JSON.stringify(result));
+                    
+                    });
+                }
+            });
+        }
+    });
+
+
+});
 
 
 // Proceso de envio de notificación por bajo consumo
+/*
 cron.schedule('22 * 23 * * *',() => {
     console.log(new Date().toString() + " Corre proceso para notificaciones");
     avisosBajoConsumo(connection, {fecha: "2021-11-04"} ,result => {
@@ -207,9 +301,11 @@ cron.schedule('22 * 23 * * *',() => {
 
     });
 });
-
-
+*/
+// -----------------------------------------------------------
 // Proceso de envio de notificación por bajo consumo
+// -----------------------------------------------------------
+
 cron.schedule('5,35 * 19 * * *',() => {
     console.log(new Date().toString() + " Corre proceso para notificaciones por bajo consumo");
     obtenInformacionMensajes(connection, {tipoNotifica: "6"} ,result => {
@@ -253,10 +349,56 @@ cron.schedule('5,35 * 19 * * *',() => {
     });
 });
 
+// ---------------------------------------------------------------------
+// Proceso de envio de notificación por Adeudo de litros "No consumidos
+// ---------------------------------------------------------------------
+
+cron.schedule('5 */5 17 * * *',() => {
+    console.log(new Date().toString() + " Corre proceso para notificaciones por adeudo de litros no consumidos");
+    obtenInformacionMensajes(connection, {tipoNotifica: "7"} ,result => {
+        console.log("dentro de obtenDatosAdeudoNoConsumo App");
+        const logName = "AdeudoNoConsumo_" + new Date().toString().split(" ").join("_")+".json";
+        console.log(result);
+        //log.setItem(logName, JSON.stringify(result));
+
+        var Datos = JSON.stringify(result);    
+        let jsonParsedArray = JSON.parse(Datos);
+        // console.log("JSON PARSED \n" + jsonParsedArray);
+        
+        for (let index = 0; index < jsonParsedArray[0].length; index++) {
+            var element = jsonParsedArray[0][index];
+            // console.log("ELEMENT \n" + JSON.stringify(element));
+
+            mailOptions.to = element.Detalle.emailConcesionario;
+            mailOptions.subject = "Notificación de adeudo por combustible no consumido " ;
+            mailOptions.template = 'AdeudoNoConsumo'
+            mailOptions.context = element.Detalle;
+            console.log(mailOptions);
+            
+            transporter.sendMail(mailOptions, (err, info) => {
+                if (err) {
+                    console.log('Correo inválido' + err.message);
+                } else {
+                    console.log("Correo enviado");
+
+                    marcaMensajeEnviado(connection, {IdMensaje: element.IdMensajeria } ,result => {
+                        console.log("marcando notificación como enviado");
+                        console.log(JSON.stringify(result));
+
+                    });
+                }
+            });
+        };
+
+    });
+});
+
+
 
 // Proceso de envio de notificación de Citas
 //cron.schedule('0 */2 * * * *',() => {
-cron.schedule('0 */50 * * * *',() => {
+/*    
+cron.schedule('0 * * * * *',() => {
     let ts = new Date();
     // console.log(new Date().toString() + " Corre proceso para notificaciones de Citas");
     console.log(ts.toString() + " Corre proceso para notificaciones de Citas");
@@ -295,6 +437,146 @@ cron.schedule('0 */50 * * * *',() => {
 
     });
 });
+*/
+// Proceso de envio de notificación de Citas
+    cron.schedule('0 */2 * * * *',() => {
+        let ts = new Date();
+        // console.log(new Date().toString() + " Corre proceso para notificaciones de Citas");
+        console.log(ts.toString() + " Corre proceso para notificaciones de Citas");
+        obtenInformacionMensajes(connection, {tipoNotifica: "1"} ,result => {
+            console.log("dentro de obtenDatosCitas App");
+            //const logName = "Citas_" + new Date().toString().split(" ").join("_")+".json";
+            console.log(result);
+            //log.setItem(logName, JSON.stringify(result));
+    
+            var Datos = JSON.stringify(result);    
+            let jsonParsedArray = JSON.parse(Datos);
+            
+            
+            for (let index = 0; index < jsonParsedArray[0].length; index++) {
+                const element = jsonParsedArray[0][index];
+                
+                element.Fecha = dateFormat(element.Detalle.Fecha, "dd/mm/yyyy HH:MM:ss")
+                element.FechaRegistro = dateFormat(element.Detalle.FechaRegistro, "dd/mm/yyyy HH:MM:ss")
+                mailOptions.to = element.Detalle.email;
+                mailOptions.subject = "Cita Cambia y Gana " ;
+                mailOptions.template = 'Cita'
+                mailOptions.context = element.Detalle;
+                console.log(mailOptions);
+                
+                transporter.sendMail(mailOptions, (err, info) => {
+                    if (err) {
+                        //result.status(500).send(error.message);
+                        console.log('Correo inválido' + err.message);
+                    } else {
+                        console.log("Correo enviado");
+                        
+                        marcaMensajeEnviado(connection, {IdMensaje: element.IdMensajeria } ,result => {
+                            console.log("marcando notificación como enviado");
+                            console.log(JSON.stringify(result));
+                        
+                        });
+                    }
+                });
+            };
+    
+        });
+    });
+    
+// ---------------------------------------------------------------------
+// Proceso de envio de notificación de Citas de Instalación
+// ---------------------------------------------------------------------
+
+cron.schedule('0 */5 * * * *',() => {
+    let ts = new Date();
+    // console.log(new Date().toString() + " Corre proceso para notificaciones de Citas");
+    console.log(ts.toString() + " Corre proceso para notificaciones de Citas de Instalación");
+    obtenInformacionMensajes(connection, {tipoNotifica: "2"} ,result => {
+        console.log("dentro de obtenDatosCitasInstalacion App");
+        
+        console.log(result);
+
+        var Datos = JSON.stringify(result);    
+        let jsonParsedArray = JSON.parse(Datos);
+        
+        for (let index = 0; index < jsonParsedArray[0].length; index++) {
+            const element = jsonParsedArray[0][index];
+            
+            element.Fecha = dateFormat(element.Detalle.Fecha, "dd/mm/yyyy HH:MM:ss")
+            element.FechaRegistro = dateFormat(element.Detalle.FechaRegistro, "dd/mm/yyyy HH:MM:ss")
+            mailOptions.to = element.Detalle.email;
+            mailOptions.subject = "Cita de instalación Cambia y Gana " ;
+            mailOptions.template = 'CitaInstalacion'
+            mailOptions.context = element.Detalle;
+            console.log(mailOptions);
+            
+            transporter.sendMail(mailOptions, (err, info) => {
+                if (err) {
+                    //result.status(500).send(error.message);
+                    console.log('Correo inválido' + err.message);
+                } else {
+                    console.log("Correo enviado");
+                    
+                    marcaMensajeEnviado(connection, {IdMensaje: element.IdMensajeria } ,result => {
+                        console.log("marcando notificación como enviado");
+                        console.log(JSON.stringify(result));
+                    
+                    });
+                }
+            });
+        };
+
+    });
+});
+
+
+// ---------------------------------------------------------------------
+// Proceso de envio de notificación de Citas de Remoción del Convertidor
+// ---------------------------------------------------------------------
+
+cron.schedule('0 */6 * * * *',() => {
+    let ts = new Date();
+    // console.log(new Date().toString() + " Corre proceso para notificaciones de Citas");
+    console.log(ts.toString() + " Corre proceso para notificaciones de Citas de Remoción");
+    obtenInformacionMensajes(connection, {tipoNotifica: "2"} ,result => {
+        console.log("dentro de obtenDatosCitasRemocion App");
+        
+        console.log(result);
+
+        var Datos = JSON.stringify(result);    
+        let jsonParsedArray = JSON.parse(Datos);
+        
+        for (let index = 0; index < jsonParsedArray[0].length; index++) {
+            const element = jsonParsedArray[0][index];
+            
+            element.Fecha = dateFormat(element.Detalle.Fecha, "dd/mm/yyyy HH:MM:ss")
+            element.FechaRegistro = dateFormat(element.Detalle.FechaRegistro, "dd/mm/yyyy HH:MM:ss")
+            mailOptions.to = element.Detalle.email;
+            mailOptions.subject = "Cita de Remoción Cambia y Gana " ;
+            mailOptions.template = 'CitaDesinstala'
+            mailOptions.context = element.Detalle;
+            console.log(mailOptions);
+            
+            transporter.sendMail(mailOptions, (err, info) => {
+                if (err) {
+                    //result.status(500).send(error.message);
+                    console.log('Correo inválido' + err.message);
+                } else {
+                    console.log("Correo enviado");
+                    
+                    marcaMensajeEnviado(connection, {IdMensaje: element.IdMensajeria } ,result => {
+                        console.log("marcando notificación como enviado");
+                        console.log(JSON.stringify(result));
+                    
+                    });
+                }
+            });
+        };
+
+    });
+});
+
+
 
 app.listen(3000, () => {
     console.log("Servidor en el puerto 3000");
